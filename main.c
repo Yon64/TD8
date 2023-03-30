33203bb0788de828f978e54f0e3aa089e6716566
Yonito <97985388+Yon64@users.noreply.github.com>
2023-03-30 12:58:43 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -4,9 +4,29 @@
 #include"soccer.h"
 
 int main(){
-  Joueur j;
-  creationJoueur(&j);
-  afficherJoueur(j);
+  Joueur j1,j2,j3,j4,j5,j6;
+  Equipe e1,e2;
+
+  creationJoueur(&j1);
+  creationJoueur(&j2);
+  creationJoueur(&j3);
+  creationJoueur(&j4);
+  creationEquipe(&e1);
+
+  ajouterJoueur(&e1,j1);
+  ajouterJoueur(&e1,j2);
+  ajouterJoueur(&e1,j3);
+  ajouterJoueur(&e1,j4);
+  afficherEquipe(e1);
+
+  creationJoueur(&j5);
+  creationJoueur(&j6);
+  creationEquipe(&e2);
+  ajouterJoueur(&e2,j5);
+  ajouterJoueur(&e2,j6);
+
+  afficherEquipe(e2);
+
 
 
   return 0;
--- soccer.c
+++ soccer.c
@@ -5,24 +5,24 @@
 
 void creationJoueur(Joueur* j){
     j->nom=malloc(100*sizeof(char));
-    printf("Nom : ");
+    printf("Nom : \n");
     scanf("%s",j->nom);
 
     j->prenom=malloc(100*sizeof(char));
-    printf("Prénom : ");
+    printf("Prénom : \n");
     scanf("%s",j->prenom);
 
     j->poste=malloc(100*sizeof(char));
-    printf("Poste : ");
+    printf("Poste : \n");
     scanf("%s",j->poste);
 
-    printf("Numero : ");
+    printf("Numero : \n");
     scanf("%d",&j->numero);
 
-    printf("Age : ");
+    printf("Age : \n");
     scanf("%d",&j->age);
 
-    printf("Nombre de but : ");
+    printf("Nombre de but : \n");
     scanf("%d",&j->nb_but);
 }
 
@@ -39,24 +39,70 @@ void afficherJoueur(Joueur j){
 
 void creationEquipe(Equipe* j){
     j->nom=malloc(100*sizeof(char));
-    printf("Nom : ");
+    printf("Nom : \n");
     scanf("%s",j->nom);
 
     j->pays=malloc(100*sizeof(char));
-    printf("Pays : ");
+    printf("Pays : \n");
     scanf("%s",j->pays);
 
     j->championnat=malloc(100*sizeof(char));
-    printf("Championnat : ");
+    printf("Championnat : \n");
     scanf("%s",j->championnat);
 
-    printf("Classement : ");
+    printf("Classement : \n");
     scanf("%d",&j->classement);
 
     j->nb_joueur=0;
+    j->mes_joueurs=malloc(1*sizeof(Joueur));
 }
 
-void ajouterJoueur(Equipe* e,Joueur j);
-void afficherEquipe(Equipe e);
-Joueur meilleurButeur(Equipe e);
-int compareEquipe(Equipe e1,Equipe e2);
+void ajouterJoueur(Equipe* e,Joueur j){
+    e->nb_joueur++;
+    e->mes_joueurs=realloc(e->mes_joueurs,e->nb_joueur*sizeof(Joueur));
+    e->mes_joueurs[e->nb_joueur-1].nom = j.nom;
+    e->mes_joueurs[e->nb_joueur-1].prenom = j.prenom;
+    e->mes_joueurs[e->nb_joueur-1].poste = j.poste;
+    e->mes_joueurs[e->nb_joueur-1].numero = j.numero;
+    e->mes_joueurs[e->nb_joueur-1].age = j.age;
+    e->mes_joueurs[e->nb_joueur-1].nb_but = j.nb_but;
+}
+
+void afficherEquipe(Equipe e){
+    printf("------------------------------------------\n");
+    printf("EQUIPE : %s\n",e.nom);
+    printf("Pays : %s\n",e.pays);
+    printf("Championnat : %s\n",e.championnat);
+    printf("Classement : %d\n",e.classement);
+    printf("Nombre de Joueurs : %d\n",e.nb_joueur);
+    for(int i=0;i<e.nb_joueur;i++){
+        afficherJoueur(e.mes_joueurs[i]);
+        printf("\n");
+    }
+}
+
+Joueur meilleurButeur(Equipe e){
+    Joueur best=e.mes_joueurs[0];
+    for (int i=1;i<e.nb_joueur-1;i++){
+        if (e.mes_joueurs[i].nb_but>best.nb_but){
+            best=e.mes_joueurs[i];
+        }
+    }
+    return best;
+}
+
+int compareEquipe(Equipe e1,Equipe e2){
+    if (e1.classement<e2.classement){
+        return 2;
+    }else{
+        if (e1.classement>e2.classement){
+            return 1;
+        }else{
+            if (e1.classement==e2.classement){
+                return 0;
+            }else{
+                return -1;
+            }
+        }
+    }
+}
